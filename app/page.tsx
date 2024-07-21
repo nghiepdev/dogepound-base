c19cd7a86c654319fac3fbd635750be691353899
NghiepDev <me@nghiep.dev>
2024-07-21 21:50:18 +0700

    Fix background

--- app/page.tsx
+++ app/page.tsx
@@ -22,35 +22,34 @@ export default function Home() {
   return (
     <main className='mx-auto max-w-screen-2xl overflow-hidden'>
       <Hero />
-
-      <div
-        className={clsx(
-          'space-y-5 sm:space-y-8 lg:space-y-10',
-          '*:multi-[-ml-10;w-[calc(100%+5rem)];rotate-[2.5deg]]',
-          'lg:*:rotate-[3.4deg]'
-        )}
-      >
-        <div>
-          <Marquee direction='left'>
-            <Image src={slide1} placeholder='blur' alt='' />
-            <Image src={slide2} placeholder='blur' alt='' />
-            <Image src={slide3} placeholder='blur' alt='' />
-            <Image src={slide4} placeholder='blur' alt='' />
-            <Image src={slide5} placeholder='blur' alt='' />
-          </Marquee>
-        </div>
-        <div>
-          <Marquee direction='right'>
-            <Image src={slide6} placeholder='blur' alt='' />
-            <Image src={slide7} placeholder='blur' alt='' />
-            <Image src={slide8} placeholder='blur' alt='' />
-            <Image src={slide9} placeholder='blur' alt='' />
-            <Image src={slide10} placeholder='blur' alt='' />
-          </Marquee>
-        </div>
-      </div>
-
       <div className='custom-gradient pb-10'>
+        <div
+          className={clsx(
+            'space-y-5 sm:space-y-8 lg:space-y-10',
+            '*:multi-[-ml-10;w-[calc(100%+5rem)];rotate-[2.5deg]]',
+            'lg:*:rotate-[3.4deg]'
+          )}
+        >
+          <div>
+            <Marquee direction='left'>
+              <Image src={slide1} placeholder='blur' alt='' />
+              <Image src={slide2} placeholder='blur' alt='' />
+              <Image src={slide3} placeholder='blur' alt='' />
+              <Image src={slide4} placeholder='blur' alt='' />
+              <Image src={slide5} placeholder='blur' alt='' />
+            </Marquee>
+          </div>
+          <div>
+            <Marquee direction='right'>
+              <Image src={slide6} placeholder='blur' alt='' />
+              <Image src={slide7} placeholder='blur' alt='' />
+              <Image src={slide8} placeholder='blur' alt='' />
+              <Image src={slide9} placeholder='blur' alt='' />
+              <Image src={slide10} placeholder='blur' alt='' />
+            </Marquee>
+          </div>
+        </div>
+
         <div className='mt-10 flex justify-center lg:mt-20'>
           <Image
             src={paperImg}
