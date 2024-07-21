93489acc1541ad404c891bde2356382c91d5450e
NghiepDev <me@nghiep.dev>
2024-07-21 21:04:21 +0700

    Add paper

--- app/page.tsx
+++ app/page.tsx
@@ -14,13 +14,22 @@ import slide8 from '@/public/slide-8.png';
 import slide9 from '@/public/slide-9.png';
 import slide10 from '@/public/slide-10.png';
 
+import paperImg from './paper.svg';
+import clsx from 'clsx';
+
 export default function Home() {
   return (
     <main className='mx-auto max-w-screen-2xl overflow-hidden'>
       <Hero />
 
-      <div className='space-y-5 sm:space-y-8 lg:space-y-10'>
-        <div className='-ml-10 w-[calc(100%+5rem)] rotate-[3.4deg]'>
+      <div
+        className={clsx(
+          'space-y-5 sm:space-y-8 lg:space-y-10',
+          '*:multi-[-ml-10;w-[calc(100%+5rem)];rotate-[2.5deg]]',
+          'lg:*:rotate-[3.4deg]'
+        )}
+      >
+        <div>
           <Marquee direction='left'>
             <Image src={slide1} placeholder='blur' alt='' />
             <Image src={slide2} placeholder='blur' alt='' />
@@ -29,7 +38,7 @@ export default function Home() {
             <Image src={slide5} placeholder='blur' alt='' />
           </Marquee>
         </div>
-        <div className='-ml-10 w-[calc(100%+5rem)] rotate-[3.4deg]'>
+        <div>
           <Marquee direction='right'>
             <Image src={slide6} placeholder='blur' alt='' />
             <Image src={slide7} placeholder='blur' alt='' />
@@ -38,6 +47,14 @@ export default function Home() {
             <Image src={slide10} placeholder='blur' alt='' />
           </Marquee>
         </div>
+      </div>
+
+      <div className='mt-10 flex justify-center lg:mt-20'>
+        <Image
+          src={paperImg}
+          alt=''
+          className='min-w-[850px] object-cover object-right'
+        />
       </div>
     </main>
   );
